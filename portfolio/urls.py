abd0802f335344cd1a5127e65a1f41aee39013ee
PokeyMcFork <pokeymcfork@gmail.com>
2022-04-25 15:30:23 +0000

    added 3 apps for proper sorting of content. Reworked index page in preparation of further development.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,4 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('about/', include('about.urls')),
+    path('contact/', include('contact.urls')),
+    path('blog/', include('blog.urls')),
+    path('work/', include('work.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
